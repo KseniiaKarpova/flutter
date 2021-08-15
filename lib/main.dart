fe81c2d55593f16c752baeae329f4a143c4e9a81
KseniiaKarpova <31700970+KseniiaKarpova@users.noreply.github.com>
2021-08-15 19:49:41 +0300

    home work 3
    HW_3

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+//import 'dart:ui';
+
+// Flutter imports:
+import 'package:flutter/material.dart';
+
+// Package imports:
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Carousel')),
+      body: Builder(
+        builder: (context) {
+          final double height = MediaQuery.of(context).size.height;
+          return CarouselSlider(
+            options: CarouselOptions(
+              height: height,
+              viewportFraction: 1.0,
+              enlargeCenterPage: false,
+              // autoPlay: false,
+            ),
+            items: imgList
+                .map((item) => Container(
+                      child: Center(
+                          child: SvgPicture.asset(item
+                              //fit: BoxFit.cover,
+                              //height: height,
+                              )),
+                    ))
+                .toList(),
+          );
+        },
+      ),
+    );
+  }
+}
+
+final List<String> imgList = [
+  'assets/images/flutter_logo.svg',
+  'assets/images/flutterio-icon.svg'
+];
